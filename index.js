cceb204229662d39a1e90d1bce5afed960a5e6e6
Santhosh Kumar S K <67744543+santhosh30108@users.noreply.github.com>
2022-10-12 15:27:11 +0530

    Adding lines 7-10 for IOS Support

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ let androidData = document.getElementById('androidData')
 let str = ""
 button.addEventListener("click",function(){
     str = text.value
+    if ('webkit' in window){
+        //webkit.messageHandlers.eventName.postMessage({parameters})
+        webkit.messageHandlers.clickHandler.postMessage({str});
+    }
     bridge.clickHandler(str);
 })
 
@@ -11,3 +15,9 @@ button.addEventListener("click",function(){
 function dataToWeb(data){
     androidData.innerText = data
 }
+
+// if (‘webkit’ in window)
+//     webkit.messageHandlers.addBookmark.postMessage({‘id’: id});
+
+// if (“webkit” in window)
+//     webkit.messageHandlers.showNotebooks.postMessage({‘id’: id, ‘isBookmarked’: isBookmarked });
